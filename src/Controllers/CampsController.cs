9cf6c01e62a8341f931f543ec5c8f70cc72a2d14
theblakeyg <theblakeyg@gmail.com>
2020-09-05 11:23:11 -0500

    Added automapper for returning model of actual data from data layer

--- src/Controllers/CampsController.cs
+++ src/Controllers/CampsController.cs
@@ -1,13 +1,39 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+using CoreCodeCamp.Data;
+using CoreCodeCamp.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace CoreCodeCamp.Controllers
 {
   [Route("api/[controller]")]
   public class CampsController : ControllerBase
   {
-    public object Get()
+    private readonly ICampRepository _repository;
+    private readonly IMapper _mapper;
+
+    public CampsController(ICampRepository repository, IMapper mapper)
     {
-      return new { Moniker = "ATL2020", Name = "Atlanta Code Camp" };
+      _repository = repository;
+      _mapper = mapper;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<CampModel[]>> GetCamps()
+    {
+      try
+      {
+        var results = await _repository.GetAllCampsAsync();
+
+        return _mapper.Map<CampModel[]>(results);
+      }
+      catch (Exception)
+      {
+        return this.StatusCode(StatusCodes.Status500InternalServerError, "Database Failure");
+      }
     }
   }
 }
