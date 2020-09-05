6b9aacda5640bd1e0f1d6a0506632cf0b8e3ecee
theblakeyg <theblakeyg@gmail.com>
2020-09-05 10:46:33 -0500

    Added first Get method in Camps Controller

---
+++ src/Controllers/CampsController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CoreCodeCamp.Controllers
+{
+  [Route("api/[controller]")]
+  public class CampsController : ControllerBase
+  {
+    public object Get()
+    {
+      return new { Moniker = "ATL2020", Name = "Atlanta Code Camp" };
+    }
+  }
+}
