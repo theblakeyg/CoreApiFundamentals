d0a445aaa5e050430eabcb6a4ea13baf702f2455
theblakeyg <theblakeyg@gmail.com>
2020-09-05 14:11:04 -0500

    Added validation to Creation of Camp

--- src/Controllers/CampsController.cs
+++ src/Controllers/CampsController.cs
@@ -84,6 +84,13 @@ namespace CoreCodeCamp.Controllers
     {
       try
       {
+        var existing = await _repository.GetCampAsync(model.Moniker);
+
+        if (existing != null)
+        {
+          return BadRequest("Moniker in use");
+        }
+
         var location = _linkGenerator.GetPathByAction("Get", "Camps", new { moniker = model.Moniker });
 
         if (string.IsNullOrEmpty(location))
